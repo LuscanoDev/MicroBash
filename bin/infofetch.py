618627fae1338f6398f83fee6561f38f4153d0b0
LexpDev <122067314+LuscanoDev@users.noreply.github.com>
2024-08-18 23:16:47 -0300

    Added some ROM informations on infofetch command

--- bin/infofetch.py
+++ bin/infofetch.py
@@ -2,6 +2,18 @@ import sys
 import os
 import micropython
 
+fs_stats = os.statvfs('/')
+
+block_size = fs_stats[0]
+total_blocks = fs_stats[2]
+free_blocks = fs_stats[3]
+
+total_size = block_size * total_blocks
+free_size = block_size * free_blocks
+used_size = total_size - free_size
+
+used_percentage = (used_size / total_size) * 100
+
 print(f"""-----InfoFetch-----
 
 Username (on bash): {username}
@@ -9,5 +21,6 @@ Device name: {os.uname().nodename}
 Machine: {os.uname().machine}
 MicroPython version: {sys.version}
 Plataform: {sys.platform}
+Memory (ROM): {round(used_size / 1000, 2)} Kb / {round(total_size / 1000, 2)} Kb ({round(used_percentage)}%) 
 
 -------------------""")
